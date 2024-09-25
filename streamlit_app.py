794adf4c10ff22cac7894c19aba1ab922299dc95
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-25 09:36:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ st.title('WT20I Performance Analysis Portal')
 
 pdf = pd.read_csv("Dataset/WT20I_Bat.csv")
 idf = pd.read_csv("Dataset/squads.csv")
+
+with st.sidebar:
+    st.header("Player Profile")
+    # Add content for Player Profile sidebar here
+
+    st.header("Matchup Analysis")
+    # Add content for Matchup Analysis sidebar here
