5c66e4676019023ce91f9f2c339fb91ebb81bccd
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-26 02:51:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -305,7 +305,7 @@ if sidebar_option == "Player Profile":
             result_df.columns = [col.upper().replace('_', ' ') for col in result_df.columns]
                     
             # Display the results
-            st.markdown("**Yearwise Performance**")
+            st.markdown(f"### **Yearwise performnce**")
             cols = result_df.columns.tolist()
 
             # Specify the desired order with 'year' first
