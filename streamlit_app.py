75180bb6bd0a450d1e7a86281a09cabfed68ddbd
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-25 17:48:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,8 @@ if sidebar_option == "Player Profile":
 
     # Filter the data for the selected player
     player_info = idf[idf['batsman'] == player_name].iloc[0]
-    ldata = ldf[ldf['batsman'] == player_name].iloc[0]
+    ldata = ldf[ldf['player-name'] == player_name].iloc[0]
+    
 
     # Tabs for "Overview", "Career Statistics", and "Current Form"
     tab1, tab2, tab3 = st.tabs(["Overview", "Career Statistics", "Current Form"])
