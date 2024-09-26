b8ede6189738a943806eb7ab03c5bf1c89cf09f7
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-26 16:29:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -258,58 +258,63 @@ if sidebar_option == "Player Profile":
                st.table(temp_df.style.set_table_attributes("style='font-weight: bold;'"))
 
         
-            # tdf = pdf[pdf['batsman'] == player_name]
+            tdf = pdf[pdf['batsman'] == player_name]
 
-            # def standardize_season(season):
-            #     if '/' in season:  # Check if the season is in 'YYYY/YY' format
-            #         year = season.split('/')[0]  # Get the first part
-            #     else:
-            #         year = season  # Use as is if already in 'YYYY' format
-            #     return year.strip()  # Return the year stripped of whitespace
-            # tdf['season'] =tdf['season'].apply(standardize_season)
+            def standardize_season(season):
+                if '/' in season:  # Check if the season is in 'YYYY/YY' format
+                    year = season.split('/')[0]  # Get the first part
+                else:
+                    year = season  # Use as is if already in 'YYYY' format
+                return year.strip()  # Return the year stripped of whitespace
+            tdf['season'] =tdf['season'].apply(standardize_season)
             
-            # # Populate an array of unique seasons
-            # unique_seasons = tdf['season'].unique()
+            # Populate an array of unique seasons
+            unique_seasons = tdf['season'].unique()
             
-            # # Optional: Convert to a sorted list (if needed)
-            # unique_seasons = sorted(set(unique_seasons))
-            # # print(unique_seasons)
-            # tdf=pd.DataFrame(tdf)
-            # # print(temp_df.head(50))
-            # tdf['batsman_runs'] = tdf['batsman_runs'].astype(int)
-            # tdf['total_runs'] = tdf['total_runs'].astype(int)
-            # # Run a for loop and pass temp_df to a cumulative function
-            # i=0
-            # for season in unique_seasons:
-            #     print(i)
-            #     temp_df = tdf[(tdf['season'] == season)]
-            #     print(temp_df.head())
-            #     temp_df = cumulator(temp_df)
-            #     if i==0:
-            #         result_df = temp_df  # Initialize with the first result_df
-            #         i=1+i
-            #     else:
-            #         result_df = pd.concat([result_df, temp_df], ignore_index=True)
-            #     result_df = result_df.drop(columns=['batsman', 'batting_team','debut_year'])
-            #     # Convert specific columns to integers
-            #     # Round off the remaining float columns to 2 decimal places
-            #     float_cols = result_df.select_dtypes(include=['float']).columns
-            #     result_df[float_cols] = result_df[float_cols].round(2)
-            # # result_df=result_df.rename(columns={'final_year':'year'})
-            # # result_df.columns = [col.upper().replace('_', ' ') for col in result_df.columns]
+            # Optional: Convert to a sorted list (if needed)
+            unique_seasons = sorted(set(unique_seasons))
+            # print(unique_seasons)
+            tdf=pd.DataFrame(tdf)
+            # print(temp_df.head(50))
+            tdf['batsman_runs'] = tdf['batsman_runs'].astype(int)
+            tdf['total_runs'] = tdf['total_runs'].astype(int)
+            # Run a for loop and pass temp_df to a cumulative function
+            i=0
+            for season in unique_seasons:
+                print(i)
+                temp_df = tdf[(tdf['season'] == season)]
+                print(temp_df.head())
+                temp_df = cumulator(temp_df)
+                if i==0:
+                    result_df = temp_df  # Initialize with the first result_df
+                    i=1+i
+                else:
+                    result_df = pd.concat([result_df, temp_df], ignore_index=True)
+                result_df = result_df.drop(columns=['batsman', 'batting_team','debut_year'])
+                # Convert specific columns to integers
+                # Round off the remaining float columns to 2 decimal places
+                float_cols = result_df.select_dtypes(include=['float']).columns
+                result_df[float_cols] = result_df[float_cols].round(2)
+                temp_df[columns_to_convert] = temp_df[columns_to_convert].fillna(0)
+                
+               # Convert the specified columns to integer type
+                temp_df[columns_to_convert] = temp_df[columns_to_convert].astype(int)
+ 
+            # result_df=result_df.rename(columns={'final_year':'year'})
+            # result_df.columns = [col.upper().replace('_', ' ') for col in result_df.columns]
                     
-            # # Display the results
-            # st.markdown(f"### **Yearwise Performnce**")
-            # cols = result_df.columns.tolist()
+            # Display the results
+            st.markdown(f"### **Yearwise Performnce**")
+            cols = result_df.columns.tolist()
 
-            # # Specify the desired order with 'year' first
-            # new_order = ['YEAR'] + [col for col in cols if col != 'YEAR']
+            # Specify the desired order with 'year' first
+            new_order = ['YEAR'] + [col for col in cols if col != 'YEAR']
                      
-            # # Reindex the DataFrame with the new column order
-            # result_df = result_df[new_order]
-            # st.table(result_df.style.set_table_attributes("style='font-weight: bold;'"))
+            # Reindex the DataFrame with the new column order
+            result_df = result_df[new_order]
+            st.table(result_df.style.set_table_attributes("style='font-weight: bold;'"))
 
-            # tdf = pdf[pdf['batsman'] == player_name]
+            tdf = pdf[pdf['batsman'] == player_name]
             # temp_df=tdf[(tdf['inning']==1)]
             # temp_df=cumulator(temp_df)
             # temp_df['inning']=1
