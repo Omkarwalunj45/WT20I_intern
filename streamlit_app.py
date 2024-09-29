723a2707e1b3373f66e0b89779dd44759056388a
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-29 10:43:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -673,7 +673,7 @@ if sidebar_option == "Player Profile":
             # Reindex the DataFrame with the new column order
             temp_df =temp_df[new_order] 
             result_df = pd.concat([result_df, temp_df], ignore_index=True)
-            result_df = result_df.drop(columns=['batsman', 'batting_team','debut_year','matches_x','matches_y','final_year','matches','batting_team'])
+            result_df = result_df.drop(columns=['batsman', 'batting_team','debut_year','matches_x','matches_y','final_year','batting_team'])
             # Convert specific columns to integers
             # Round off the remaining float columns to 2 decimal places
             float_cols = result_df.select_dtypes(include=['float']).columns
