9d344ddfc6931b7bae5ba55cedfbd143c6b83bd6
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-29 18:37:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,8 @@ def show_match_details(match_id):
             total_balls_1 = innings_1['ball'].count()
             overs_1 = total_balls_1 // 6
             balls_1 = total_balls_1 % 6
-            st.write(f"{innings_1['batting_team'].iloc[0]} Innings: {total_runs_1}/{innings_1['is_wkt'].sum()} ({overs_1}.{balls_1} ov)",fontsize=14)
+            st.markdown(f"<h3 style='font-size: 14px;'>{innings_1['batting_team'].iloc[0]} Innings: {total_runs_1}/{innings_1['is_wkt'].sum()} ({overs_1}.{balls_1} ov)</h3>", unsafe_allow_html=True)
+
 
             show_innings_scorecard(innings_1, f"Innings 1: {batting_team_1} Women")
         if not innings_2.empty:
@@ -55,7 +56,8 @@ def show_match_details(match_id):
             total_balls_2 = innings_2['ball'].count()
             overs_2 = total_balls_2 // 6
             balls_2 = total_balls_2 % 6
-            st.write(f"{innings_2['batting_team'].iloc[0]} Innings: {total_runs_2}/{innings_2['is_wkt'].sum()} ({overs_2}.{balls_2} ov)",fontsize=14)
+            st.markdown(f"<h3 style='font-size: 14px;'>{innings_2['batting_team'].iloc[0]} Innings: {total_runs_2}/{innings_2['is_wkt'].sum()} ({overs_2}.{balls_2} ov)</h3>", unsafe_allow_html=True)
+
             show_innings_scorecard(innings_2, f"Innings 2: {batting_team_2} Women")
     else:
         st.write("No match details found.")
