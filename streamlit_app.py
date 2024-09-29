39c5e3dbee059cedbcd0a73e4f5bcdda477088c8
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-29 10:43:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -561,7 +561,7 @@ if sidebar_option == "Player Profile":
                     result_df = pd.concat([result_df, temp_df], ignore_index=True)
             
             # Display the final result_df
-            result_df = result_df.drop(columns=['matches_x','matches_y','batsman','debut_year','final_year','matches','batting_team'])
+            result_df = result_df.drop(columns=['matches_x','matches_y','batsman','debut_year','final_year','batting_team'])
             result_df.columns = [col.upper().replace('_', ' ') for col in result_df.columns]
             columns_to_convert = ['HUNDREDS', 'FIFTIES','THIRTIES', 'RUNS','HIGHEST SCORE']
 
