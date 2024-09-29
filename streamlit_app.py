d4fbf8e4990560ec3b0ca32541ff7fa1c55b819b
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-29 10:46:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -983,7 +983,7 @@ if sidebar_option == "Player Profile":
                     result_df = pd.concat([result_df, temp_df], ignore_index=True)
             
             if 'bowler' in result_df.columns:
-                result_df = result_df.drop(columns=['bowler','debut_year','final_year','matches','batting_team'])
+                # result_df = result_df.drop(columns=['bowler','debut_year','final_year','matches','batting_team'])
             result_df.columns = [col.upper().replace('_', ' ') for col in result_df.columns]
             columns_to_convert = ['RUNS','THREE WICKET HAULS', 'MAIDEN OVERS']
 
