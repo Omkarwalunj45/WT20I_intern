44edcae64535591dedd195512f21dbe0eb0cc121
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-29 14:32:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -994,8 +994,8 @@ if sidebar_option == "Player Profile":
             new_order = ['MATCH ID','DATE'] + [col for col in cols if col != ['MATCH ID','DATE']]          
             current_form_df = current_form_df[new_order] 
             columns_to_convert = ['MATCH ID']
-            result_df[columns_to_convert] = result_df[columns_to_convert].astype(int)
-            current_form_df=current_form_df.sort_values('MATCH ID',ascending=False)
+            # result_df[columns_to_convert] = result_df[columns_to_convert].astype(int)
+            # current_form_df=current_form_df.sort_values('MATCH ID',ascending=False)
             # st.table(current_form_df[['Match ID', 'Runs', 'Balls Faced', 'SR','Balls Bowled','Runs Given','Wickets', 'Econ','Venue']])
             current_form_df = current_form_df.loc[:, ~current_form_df.columns.duplicated()]
             st.table(current_form_df.style.set_table_attributes("style='font-weight: bold;'"))
