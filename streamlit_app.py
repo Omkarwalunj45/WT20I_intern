9361098174c78e4622d3fe77d5dd91f82ab35ff6
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-30 17:42:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ info_df=pd.read_csv("Dataset/player_info_k.csv",low_memory=False)
 bpdf=pd.read_csv("Dataset/Mydataset.csv",low_memory=False)
 bidf=pd.read_csv("Dataset/lifesaver_bowl.csv",low_memory=False)
 info_df=info_df.rename(columns={'Player':'Player_name'})
+pdf[['noballs', 'wides']] = pdf[['noballs', 'wides']].fillna(0)
 pdf['valid_ball'] = pdf.apply(lambda x: 1 if (x['wides'] == 0 and x['noballs'] == 0) else 0, axis=1)
 
 
@@ -129,7 +130,7 @@ def show_innings_scorecard(inning_data, title):
     # Bowling scorecard
     st.write("Bowling")
     bowling_data = inning_data.groupby(['bowler']).agg({
-        'valid_ball': 'count',
+        'valid_ball': 'sum',
         'total_runs': 'sum',
         'is_wkt': 'sum',
         'wides': 'sum',
