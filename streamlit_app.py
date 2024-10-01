796e36a8244084636a6f684b4b6b27f61b907ded
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-10-01 13:11:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ st.set_page_config(page_title='WT20I Performance Analysis Portal', layout='wide'
 st.title('WT20I Performance Analysis Portal')
 
 # # Load data
+
 pdf = pd.read_csv("Dataset/Mydataset.csv",low_memory=False)
 idf = pd.read_csv("Dataset/lifesaver_bat.csv",low_memory=False)
 info_df=pd.read_csv("Dataset/player_info_k.csv",low_memory=False)
