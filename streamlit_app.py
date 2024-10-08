e92685bb777dbb59e3512626f1d357983c38f5d2
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-10-08 23:35:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title='WT20I Performance Analysis Portal', layout='wide')
 st.title('WT20I Performance Analysis Portal')
 pdf = pd.read_csv("Dataset/Mydataset.csv",low_memory=False)
-pdfn = pd.read_csv("Dataset/ICC_WT20I_2024.csv",low_memory=False)
+pdfn = pd.read_csv("Dataset/fant.csv",low_memory=False)
 idf = pd.read_csv("Dataset/lifesaver_bat.csv",low_memory=False)
 info_df=pd.read_csv("Dataset/player_info_k.csv",low_memory=False)
 bpdf=pd.read_csv("Dataset/Mydataset.csv")
-bpdfn = pd.read_csv("Dataset/ICC_WT20I_2024.csv",low_memory=False)
+bpdfn = pd.read_csv("Dataset/fant.csv",low_memory=False)
 bidf=pd.read_csv("Dataset/lifesaver_bowl.csv",low_memory=False)
 info_df=info_df.rename(columns={'Player':'Player_name'})
 cols_conv=['season','match_id']
