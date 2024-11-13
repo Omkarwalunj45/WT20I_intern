2e83d78d20eb6e9ee09e1608774747105deb02cc
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-11-13 08:58:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3010,7 +3010,7 @@ else :
         }
         
         # Function to apply a small random offset while checking pitch boundaries
-        def apply_offset(value, offset_range=(-0.0, 0.0), boundary=(-0.5, 0.5)):
+        def apply_offset(value, offset_range=(-0.05, 0.05), boundary=(-0.5, 0.5)):
             offset = np.random.uniform(offset_range[0], offset_range[1])
             if boundary[0] <= value + offset <= boundary[1]:
                 return value + offset
