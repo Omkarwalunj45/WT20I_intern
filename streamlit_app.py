1e501f1b112ea3ffb5c68f6737b2391159c6d8cb
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-11-14 14:19:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2897,7 +2897,7 @@ else :
                         <span class="bold">4s:</span> {int(total_fours)} 🟨 ({percent_fours:.1f}%) | 
                         <span class="bold">6s:</span> {int(total_sixes)} 🟫 ({percent_sixes:.1f}%)
                     </div>
-                    if {int(total_runs)} >= 80:
+                    if total_runs >= 80:
                         <div class="highlight">Most Productive Shot: {max_shot}</div>
                     <div class="highlight">False Shot %: {false_shot_percentage:.1f}%</div>
                 </div>
